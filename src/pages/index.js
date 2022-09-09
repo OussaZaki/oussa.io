234d7586ed97e62e4e8b12bc9d7e3285f87484ee
Oussama Zaki <oussaz@spotify.com>
2022-09-09 15:58:19 +0200

    🎵 spotify update

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,23 +10,39 @@ class ProfilePage extends React.Component {
   render() {
     return (
       <div className="profile-container">
-        <SEO title="Oussama Zaki · Technology Leader" />
+        <SEO title="Oussa · Art & Tech" />
         <section id="s1">
           <div className="container">
-            <h1 className="center">Oussama Zaki.</h1>
-            <h2 className="center yellow">Technology Leader.</h2>
+            <h1 className="center">Oussa.</h1>
+            <h2 className="center">Art & Tech</h2>
           </div>
         </section>
 
         <section id="s2">
           <div className="container">
+            <p>Hi,</p>
             <p>
-              I find meaning in life through impact, the ensemble of values I
-              bring to my environment and my surrounding. By that definition, my
-              goal in life can be summarized into two words,{" "}
-              <span className="yellow">Positive Impact.</span> & I strongly
-              believe that technology can help me get closer to that goal.
+              My name is Oussama Zaki — Oussa for short, I'm currently based in
+              Stockholm, helping internal teams at Spotify run large scale
+              experiments; as a Senior Engineer.
             </p>
+            <p>
+              Originally from the Computer Science sphere, where I spent most of
+              my days fiddling with Combinatorial Optimisation Algorithms &
+              Heuristics.
+            </p>
+            <p>
+              I progressively started moving towards the border line of
+              human-computer interactions; while fostering a profound Passion
+              for UX & Design, across different experiences at Google, Klarna,
+              Svea Solar...
+            </p>
+            <p>
+              Always down to discuss or collaborate on topics around Art, UX &
+              Engineering. You can also reach out if you are in need of some
+              nice Omelette tips and recipes.
+            </p>
+            <p>Love,</p>
           </div>
         </section>
         <section id="s3">
